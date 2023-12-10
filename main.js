33fa8064889a9a8abbf21350e17314cc847448a1
Shazid <shazid.dev@gmail.com>
2023-12-10 20:02:31 +0600

    added submenu

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const sidemenuBtn = document.getElementById("sidemenu-btn");
 const sidemenu = document.querySelector(".sidemenu-area");
 const closeBtn = document.getElementById("sidemenu-close-btn");
 const overlay = document.querySelector(".sidemenu-overlay");
+const dropdownToggle = document.querySelectorAll(".cp-dropdown-toggle");
 
 sidemenuBtn.addEventListener("click", () => {
   sidemenu.classList.add("active");
@@ -10,6 +11,10 @@ sidemenuBtn.addEventListener("click", () => {
 function menuClose() {
   sidemenu.classList.remove("active");
 }
-
+dropdownToggle.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log(e.target);
+  });
+});
 closeBtn.addEventListener("click", menuClose);
 overlay.addEventListener("click", menuClose);
