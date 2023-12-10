4ea12272b34eaa7de86f53ee38167c138e3ab8c1
Shazid <shazid.dev@gmail.com>
2023-12-10 18:23:49 +0600

    functionality added

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const sidemenuBtn = document.getElementById("sidemenu-btn");
+const sidemenu = document.querySelector(".sidemenu-area");
+const closeBtn = document.getElementById("sidemenu-close-btn");
+const overlay = document.querySelector(".sidemenu-overlay");
+
 sidemenuBtn.addEventListener("click", () => {
-  console.log("clicked");
+  sidemenu.classList.add("active");
 });
+
+function menuClose() {
+  sidemenu.classList.remove("active");
+}
+
+closeBtn.addEventListener("click", menuClose);
+overlay.addEventListener("click", menuClose);
